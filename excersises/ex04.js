ead41984e0cd9fda7a03ec0f6525606d12acdfe6
Ashley lau <68827831+yuma738@users.noreply.github.com>
2025-10-14 20:51:15 -0700

    Add files via upload

---
+++ excersises/ex04.js
@@ -0,0 +1,30 @@
+// add a button titled click me 
+// when it is clicked
+let totoalNumber = 0; 
+$("needy-button").html("new text"); 
+
+$("#needy-button").click( function() {
+    totoalNumber = totoalNumber + 1; 
+    count = count + 1; 
+    let sentence = "I was clicked"
+    let totalnumbers = sentence + totoalNumber; 
+    $("#needy-button"). html("you clicked me" + count + "times"); 
+    $("#needy-button").html(totalnumbers); 
+    let sentencetwo = "oh no"; 
+    let totoalNumber = sentence +totoalNumber + sentencetwo; 
+
+});
+    
+
+
+
+
+// show how many times 
+// add one to the total number 
+// on our  button 
+// show "clicked totanal number times "
+
+// a top limit 
+
+
+// 
